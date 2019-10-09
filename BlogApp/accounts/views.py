cb1b1055b05c66f27539e1c412122fabe36808b0
Usman Ali <usmanusman136@hotmail.com>
2019-10-09 16:38:26 +0500

    adding css and http get request

--- BlogApp/accounts/views.py
+++ BlogApp/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import path
 from . import views 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate
@@ -12,35 +12,43 @@ from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    if request.method=='POST':
+    if request.user.is_authenticated:
+        return redirect('feed')
+    elif request.method=='POST':
+
         email=request.POST['email']
         password=request.POST['password']
-
         user = authenticate(username=email, password=password)
         if user is not None:
-            messages.error(request,'Login Success')
-            return redirect('feed')
-            print("Success")
+            auth.login(request,user)
+            return redirect('feed') 
         else:
-            messages.error(request,'username or password not correct')
+            messages.error(request,'email or password not correct')
             return redirect('login')
-            print("Error")
     else:
         return render(request,'accounts/login.html')
-
+                
 def register(request):
-    
-    if request.method=='POST':
+    if request.user.is_authenticated:
+        return redirect('feed')
+  
+
+    elif request.method=='POST':
         name=request.POST['name']
         email=request.POST['email']
         password=request.POST['password']
-  
+    
         user = User.objects.create_user(password=password, username=email, first_name=name)
         status=user.save()
         print(status)
         return redirect('login')
     else:
         return render(request,'accounts/register.html')
-        
 
-        
+def logout(request):
+    if request.method =='POST':
+        auth.logout(request)
+        messages.success(request,'You are now logged out')
+        return redirect ('login')
+    else:
+        return
--- BlogApp/feed/views.py
+++ BlogApp/feed/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views 
+from django.contrib.auth.models import User,auth
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib import messages
+import requests
+
 
 # Create your views here.
 def feed(request):
-    return render(request,'feeds/feed.html')
+    if request.user.is_authenticated:
+        response = requests.get('https://sbuf52bt6i.execute-api.us-east-2.amazonaws.com/Test/drax-test?type=posts')
+        postdata=response.json()
+        print(postdata)
+
+        context={
+            'postss':postdata
+        }
+
+        return render(request,'feeds/feed.html',context)
+    else:
+        return redirect('login')
 def singlefeed(request):
-    return render(request,'feeds/singlefeed.html')
+
+    if request.user.is_authenticated:
+        return render(request,'feeds/singlefeed.html')
+    else:
+        return redirect('login')
 def createfeed(request):
-    return render(request,'feeds/createfeed.html')
+    if request.user.is_authenticated:
+        return render(request,'feeds/createfeed.html')
+    else:
+        return redirect('login')
+    
