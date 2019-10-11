c6f8a228a03063ceddaf37a88da0eb48692f7f97
Usman Ali <usmanusman136@hotmail.com>
2019-10-11 12:30:34 +0500

    added the posts to the page

--- BlogApp/feed/views.py
+++ BlogApp/feed/views.py
@@ -7,20 +7,23 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate
 from django.contrib import messages
 import requests
-
+from .models import Feeds
 
 # Create your views here.
 def feed(request):
     if request.user.is_authenticated:
-        response = requests.get('https://sbuf52bt6i.execute-api.us-east-2.amazonaws.com/Test/drax-test?type=posts')
-        postdata=response.json()
-        print(postdata)
+        # response = requests.get('https://sbuf52bt6i.execute-api.us-east-2.amazonaws.com/Test/drax-test?type=posts')
+        # postdata=response.json()
+        # print(postdata)
+        if request.method=='POST':
+            return
 
-        context={
-            'postss':postdata
-        }
-
-        return render(request,'feeds/feed.html',context)
+        else:
+            feed=Feeds.objects.all().order_by('-DateTime')
+            context={
+                'feed':feed
+            }
+            return render(request,'feeds/feed.html',context)
     else:
         return redirect('login')
 def singlefeed(request):
