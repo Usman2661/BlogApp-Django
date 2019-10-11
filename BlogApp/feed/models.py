2b79e88f89f5afc261d0511fbe9bd57665e26201
Usman Ali <usmanusman136@hotmail.com>
2019-10-11 11:21:05 +0500

    Feeds Migration

--- BlogApp/feed/models.py
+++ BlogApp/feed/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
+from  django.contrib.auth.models import User
 
 # Create your models here.
+class Feeds(models.Model):
+    UserID = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    PostTitle= models.CharField(max_length=200)
+    PostMessage=models.TextField(blank=True)
+    Image = models.ImageField(upload_to='photos', blank=False)
+    Catagory=models.CharField(max_length=200)
+    DateTime=models.DateTimeField(default=datetime.now,blank =False)
+
+    def __str__(self):
+        return self.PostTitle
