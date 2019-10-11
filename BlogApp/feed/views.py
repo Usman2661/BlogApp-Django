20ea1a1ed8cd3b886a942e987d3af8b024cdf07e
Usman Ali <usmanusman136@hotmail.com>
2019-10-11 14:25:51 +0500

    Migration for comments updated the singlepost page

--- BlogApp/feed/views.py
+++ BlogApp/feed/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import authenticate
 from django.contrib import messages
 import requests
 from .models import Feeds
+from comment.models import Comments
 
 # Create your views here.
 def feed(request):
@@ -16,8 +17,16 @@ def feed(request):
         # postdata=response.json()
         # print(postdata)
         if request.method=='POST':
-            return
 
+            comment=request.POST['Comment']
+            PostID=request.POST['PostID']
+            UserID=request.user.id
+            print(UserID)
+
+            mycomment=Comments.objects.create(Comment=comment, PostID=PostID, UserID_id=UserID)
+            mycomment.save()
+
+            return redirect('feed')
         else:
             feed=Feeds.objects.all().order_by('-DateTime')
             context={
@@ -29,7 +38,29 @@ def feed(request):
 def singlefeed(request):
 
     if request.user.is_authenticated:
-        return render(request,'feeds/singlefeed.html')
+
+
+        if request.method=='POST':
+
+            comment=request.POST['Comment']
+            PostID=request.POST['PostID']
+            # UserID=request.user.id
+            # print(UserID)
+            UserID=2
+
+            mycomment=Comments.objects.create(Comment=comment, PostID=3, UserID_id=2)
+            mycomment.save()
+
+            return redirect('singlefeed?Post='+PostID)
+        else:
+            PostID=request.GET.get('Post')
+            feed=Feeds.objects.get(pk=PostID)
+            comment=Comments.objects.filter(PostID=PostID)
+            context={
+                'feed':feed,
+                'comment':comment
+            }
+            return render(request,'feeds/singlefeed.html',context)
     else:
         return redirect('login')
 def createfeed(request):
